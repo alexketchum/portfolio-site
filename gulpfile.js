a5a47703c24d1902da83944e1a79156d07bb057e
Alex Ketchum <aketchum2112@gmail.com>
2017-08-26 22:37:55 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    cssnano      = require('gulp-cssnano'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browsersync  = require('browser-sync');
+
+// start browsersync
+gulp.task('browserSync', function() {
+    browsersync({
+        server: {
+            baseDir: 'app'
+        }   
+    })
+});
+
+//create workflow task
+gulp.task('workflow', function() {
+    gulp.src('./app/sass/**/*.scss')
+        .pipe(sourcemaps.init()) // initiate sourcemaps
+        .pipe(sass().on('error', sass.logError)) // initiate sass with error logging
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        })) // autoprefix to last two browser versions
+        .pipe(cssnano()) // set up minifying
+        .pipe(sourcemaps.write('./')) // write sourcemaps
+    .pipe(gulp.dest('./app/css/'))
+    // refresh
+    .pipe(browsersync.reload({
+        stream: true
+    }));
+});
+
+gulp.task('default', ['browserSync', 'workflow'], function() {
+    gulp.watch('./app/sass/**/*.scss', ['workflow']);
+    gulp.watch('./app/index.html', function() {
+        browsersync.reload();
+    });
+});
